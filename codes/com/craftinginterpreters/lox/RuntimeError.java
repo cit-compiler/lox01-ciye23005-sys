835ecb59138d7a89e836707a83f87a17b2356f88
ciye23005 <ciye23005@g.nihon-u.ac.jp>
2026-01-13 09:40:17 +0900

    fixed RuntimeError.java

--- codes/com/craftinginterpreters/lox/RuntimeError.java
+++ codes/com/craftinginterpreters/lox/RuntimeError.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-public class RuntimeError extends RuntimeException {
+class RuntimeError extends RuntimeException {
     final Token token;
 
     RuntimeError(Token token, String message) {
